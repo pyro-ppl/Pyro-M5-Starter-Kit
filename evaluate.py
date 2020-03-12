3ee699501cfe939bc663a6d4c2fce1e763ef7678
fehiepsi <fehiepsi@gmail.com>
2020-03-12 15:09:50 -0400

    add min_train_window

--- evaluate.py
+++ evaluate.py
@@ -3,19 +3,10 @@
 
 import torch
 
-from pyro.contrib.forecast import backtest as pyro_backtest
-from pyro.ops.stats import crps_empirical, quantile
+from pyro.contrib.forecast.evaluate import backtest, eval_crps, eval_mae, logger
+from pyro.ops.stats import quantile
 
 from util import M5Data
-
-
-@torch.no_grad()
-def eval_mae(pred, truth):
-    """
-    Like pyro.contrib.forecast.eval_mae but does not average over batch dimensions.
-    """
-    pred = pred.median(0).values
-    return (pred - truth).abs().reshape(truth.shape[:-2] + (-1,)).mean(-1).cpu()
 
 
 @torch.no_grad()
@@ -25,15 +16,7 @@ def eval_rmse(pred, truth):
     """
     pred = pred.mean(0)
     error = pred - truth
-    return (error * error).reshape(truth.shape[:-2] + (-1,)).mean(-1).sqrt().cpu()
-
-
-@torch.no_grad()
-def eval_crps(pred, truth):
-    """
-    Like pyro.contrib.forecast.eval_crps but does not average over batch dimensions.
-    """
-    return crps_empirical(pred, truth).reshape(truth.shape[:-2] + (-1,)).mean(-1).cpu()
+    return (error * error).reshape(truth.shape[:-2] + (-1,)).mean(-1).sqrt()
 
 
 @torch.no_grad()
@@ -46,7 +29,7 @@ def eval_pl(pred, truth):
     error = pred - truth.unsqueeze(0)
     us = us.reshape((-1,) + (1,) * (pred.dim() - 1)).expand(pred.shape)
     error = (torch.where(error <= 0, us, 1 - us) * error).mean(0)  # mean accross all quantiles
-    return error.reshape(truth.shape[:-2] + (-1,)).mean(-1).cpu()
+    return error.reshape(truth.shape[:-2] + (-1,)).mean(-1)
 
 
 DEFAULT_METRICS = {
@@ -60,20 +43,20 @@ DEFAULT_METRICS = {
 def eval_wrmsse(raw_value, train_data, weight):
     lag1 = train_data[..., 1:, :] - train_data[..., :-1, :]
     # drop starting 0s
-    actual_length = (train_data.cumsum(-2) != 0).sum(-2)
-    scale = lag1.pow(2).sum(-2).div(actual_length - 1).sqrt()
-    return (weight * raw_value / scale).sum().item()
+    actual_length = (train_data.sum(-1).cumsum(-1) != 0).sum(-1)
+    scale = lag1.pow(2).mean(-1).sum(-1).div(actual_length - 1).sqrt()
+    return (weight * raw_value / scale).sum().cpu().item()
 
 
 def eval_wspl(raw_value, train_data, weight):
     lag1 = train_data[..., 1:, :] - train_data[..., :-1, :]
     # drop starting 0s
-    actual_length = (train_data.cumsum(-2) != 0).sum(-2)
-    scale = lag1.abs().sum(-2).div(actual_length - 1)
-    return (weight * raw_value / scale).sum().item()
+    actual_length = (train_data.sum(-1).cumsum(-1) != 0).sum(-1)
+    scale = lag1.abs().mean(-1).sum(-1).div(actual_length - 1)
+    return (weight * raw_value / scale).sum().cpu().item()
 
 
-def backtest(data, covariates, model_fn, weight=None, **kwargs):
+def m5_backtest(data, covariates, model_fn, weight=None, **kwargs):
     """
     Backtest function with weighted metrics. See
     http://docs.pyro.ai/en/stable/contrib.forecast.html#pyro.contrib.forecast.evaluate.backtest
@@ -88,14 +71,23 @@ def backtest(data, covariates, model_fn, weight=None, **kwargs):
     if weight is None:
         weight = data.new_ones(data.shape[:-1])
     assert weight.shape == data.shape[:-1]
-    weight = weight / weight.sum()
+    # normalize over batch dimensions
+    weight = weight / weight.reshape((-1, data.shape[-2])).sum(0)
 
     if kwargs.get("metrics") is None:
         kwargs["metrics"] = DEFAULT_METRICS
 
-    windows = pyro_backtest(data, covariates, model_fn, **kwargs)
+    min_train_window = (data.sum(-1).cumsum(-1) == 0).sum(-1).max().cpu().item() + 2
+    if kwargs.get("min_train_window", 1) < min_train_window:
+        logger.info(f"min_train_window is set to {min_train_window} "
+                    "to be able to compute scaled metrics.")
+        kwargs["min_train_window"] = min_train_window
+
+    windows = backtest(data, covariates, model_fn, **kwargs)
     for window in windows:
-        train_data = data[..., window["t0"]:window["t1"], :]
+        # we use all historical data before t1 to compute
+        # the scale factor of wrmsse and wspl
+        train_data = data[..., :window["t1"], :]
         weight = weight[window["t1"] - 1]
 
         if "rmse" in window:
