05d241982fb3fe4c425212c7f9bb503a9ff8d157
fehiepsi <fehiepsi@gmail.com>
2020-04-05 22:52:07 -0400

    add comments

--- model3.py
+++ model3.py
@@ -2,11 +2,11 @@
 # SPDX-License-Identifier: Apache-2.0
 
 """
-Middle-out model
+Middle-out Model
 ================
 
-This script gives an example on how to use Pyro forecast module to backtest and
-make a submission for M5 accuracy/uncertainty competition.
+This example model illustrates how to use M5Data class to aggregate,
+disaggregate data or prediction at each aggregation levels.
 
 Using the middle-out approach in [1], we first construct a model to predict
 the aggregated sales across stores and departments. Then we will distribute
@@ -40,6 +40,8 @@ if not os.path.exists(RESULTS):
     os.makedirs(RESULTS)
 
 
+# We will use a model similar to the one defined in `model1.py`. But instead of working
+# with the top level data, we will use 70 timeseries at store+department level here.
 class Model(ForecastingModel):
     def model(self, zero_data, covariates):
         num_stores, num_depts, duration, one = zero_data.shape
@@ -78,7 +80,10 @@ def main(args):
     level = ["store_id", "dept_id"]
     data = m5.get_aggregated_sales(level)
     data = data.reshape(m5.num_stores, m5.num_depts, m5.num_train_days, 1)
-    # scale data to the evaluation metric
+    # each department at each store has different scales; instead of
+    # using log transform as in `model1.py`, we will scale those timeseries down
+    # using the same scale of WSPL evaluation metric. Please refer to
+    # evaluation section of M5 guideline for more information about scale.
     scale = get_metric_scale("pl", data).unsqueeze(-1).unsqueeze(-1)
     data = data / scale
 
@@ -88,6 +93,8 @@ def main(args):
     time = torch.arange(float(T2), device="cpu") / 365
     covariates = torch.cat([
         time.unsqueeze(-1),
+        # The follow code creates yearly features so we can learn yearly
+        # pattern from the dataset.
         periodic_features(T2, 365.25, 7)
     ], dim=-1)
 
@@ -106,15 +113,25 @@ def main(args):
     }
 
     def transform(pred, truth):
+        # note that our pred/truth are results at store+dept level;
+        # we need to aggregate them to higher aggregation levels
+        # to verify if the result at higher aggregative levels is still good
+
         pred, truth = (pred.clamp(min=0) * scale).cpu(), (truth * scale).cpu()
         num_samples, duration = pred.size(0), pred.size(-2)
+        # Note that the method `m5.aggregate_samples` only aggregates non-aggregated
+        # timeseries to higher levels, but we have timeseries at the middle store+dept
+        # level. So we assume the all items in each department at each store have the
+        # same sales, disaggregate the timeseries at store+dept level to the
+        # non-aggregated level. Then aggretates the result to any higher aggregation level.
         num_items_by_dept = torch.tensor(m5.num_items_by_dept, device="cpu")
         pred = pred / num_items_by_dept.unsqueeze(-1).unsqueeze(-1)
         non_agg_pred = pred.repeat_interleave(num_items_by_dept, dim=-3)
         non_agg_pred = non_agg_pred.reshape(num_samples, -1, duration)
+        # note that store+dept is the forth level from bottom up.
         agg_pred = m5.aggregate_samples(non_agg_pred, *m5.aggregation_levels[:-3])
 
-        # similarly
+        # similarly, we apply the same procedure for truth data
         truth = truth / num_items_by_dept.unsqueeze(-1).unsqueeze(-1)
         non_agg_truth = truth.repeat_interleave(num_items_by_dept, dim=-3)
         non_agg_truth = non_agg_truth.reshape(1, -1, duration)
@@ -132,17 +149,22 @@ def main(args):
                              num_samples=1000, batch_size=10)
         samples = samples.clamp(min=0) * scale
 
-        # compute the ratio of prediction w.r.t. the sales of last 28 days
-        num_items_by_dept = torch.tensor(m5.num_items_by_dept)
+        # Compute the ratio of prediction w.r.t. the sales of last 28 days
+        # first, we get the total sales of each department at each store in the last 28 days
         dept_store_sales = m5.get_aggregated_sales(level)[:, -28:].sum(-1)
         dept_store_sales = dept_store_sales.reshape(m5.num_stores, m5.num_depts)
+        # num_items_by_dept tells us how many items in each department
+        num_items_by_dept = torch.tensor(m5.num_items_by_dept)
         dept_store_sales = dept_store_sales.repeat_interleave(num_items_by_dept, dim=-1)
+        # get the sales at the lowest level: store+item (this is the non-aggregated level)
         sales_last28 = m5.get_aggregated_sales(["store_id", "item_id"])[:, -28:].sum(-1)
         proportion = sales_last28 / dept_store_sales.reshape(-1)
 
+        # after calculate the ratio, we disaggregate prediction to the bottom level
         samples = samples.squeeze(-1).repeat_interleave(num_items_by_dept, dim=-2)
         samples = samples.reshape(-1, m5.num_timeseries, 28)
         non_agg_samples = torch.poisson(samples * proportion.unsqueeze(-1))
+        # aggregate the result to all aggregation levels
         agg_samples = m5.aggregate_samples(non_agg_samples, *m5.aggregation_levels)
         # cast to numpy because pyro quantile implementation is memory hungry
         print("Calculate quantiles...")
@@ -151,7 +173,7 @@ def main(args):
         filename, ext = os.path.splitext(args.output_file)
         m5.make_uncertainty_submission(filename + "_uncertainty" + ext, q, float_format='%.3f')
     else:
-        # calculate weight of each timeseries
+        # calculate weight of each timeseries higher or equal to store+department level
         weight = m5.get_aggregated_ma_dollar_sales(m5.aggregation_levels[-1]).cpu()
         weight = weight / weight.sum(0, keepdim=True)
         agg_weight = m5.aggregate_samples(weight.unsqueeze(0), *m5.aggregation_levels[:-3])
@@ -182,7 +204,7 @@ def main(args):
 if __name__ == "__main__":
     assert pyro.__version__ >= "1.3.0"
     parser = argparse.ArgumentParser(description="Univariate M5 daily forecasting")
-    parser.add_argument("--num-windows", default=1, type=int)
+    parser.add_argument("--num-windows", default=3, type=int)
     parser.add_argument("--test-window", default=28, type=int)
     parser.add_argument("-s", "--stride", default=35, type=int)
     parser.add_argument("-n", "--num-steps", default=2001, type=int)
